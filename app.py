4154b60a895734bbfa9590dade0e6828db12523c
Tanusree Nagesh <tanunageshh@gmail.com>
2026-07-05 17:00:10 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Predictive Forecasting of Care Load & Placement Demand")
+
+st.write("This project forecasts Children in HHS Care using machine learning and time-series forecasting models.")
+
+st.write("Models Used:")
+st.write("- Random Forest")
+st.write("- Gradient Boosting")
+st.write("- ARIMA")
+st.write("- SARIMA")
+st.write("- Exponential Smoothing")
+
+st.success("Project deployed successfully.")
